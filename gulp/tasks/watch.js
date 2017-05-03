02a7dde8f32da70179b06240fbea4019e1aab9dd
Ben <benvenda@gmail.com>
2017-05-02 21:45:33 -0400

    Completed large hero block and intro content block

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,29 @@
+var gulp        = require('gulp'),
+watch           = require('gulp-watch'),
+browserSync     = require('browser-sync').create();
+
+gulp.task('watch', function() {
+
+    browserSync.init({
+        notify: false,  // Supresses notification on updates
+        server: {
+            baseDir: "app"
+        }
+    });
+
+    watch('./app/index.html', function() {
+        browserSync.reload();
+    });
+
+    watch('./app/assets/styles/**/*.css', function() {
+        gulp.start('cssInject');
+    })
+
+});
+
+// Middle argument is 'dependencies': tasks which must be run
+//  before the function is called.
+gulp.task('cssInject', ['styles'], function() {
+    return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
+});
