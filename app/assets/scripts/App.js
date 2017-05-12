03c15b7aace33ac07bf2bbb285d4eccec1814a90
Ben <benvenda@gmail.com>
2017-05-12 16:35:49 -0400

    Finished the header section.

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,6 @@
+function person(name, colour) {
+    console.log("Hello, my name is " + name + " and my favourite colour is " + colour + ".");
+}
+
+person("John Doe", "blue");
+person("Jane Smith", "green");
